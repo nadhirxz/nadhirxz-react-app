44ca24b20675f93e52d661a28049c88499ed09bb
Nadhir <mamokaze@gmail.com>
2020-12-05 01:25:00 +0100

    Fix

--- src/components/Dot.js
+++ src/components/Dot.js
@@ -1,14 +1,7 @@
 import React, { Component } from 'react'
 
 export default class Dot extends Component {
-    getStyle = () => {
-        return {
-            backgroundColor: this.props.color
-        }
-    }
     render() {
-        return (
-            <div className="dot" style={this.getStyle()}></div>
-        )
+        return (<div className="dot" style={{backgroundColor: this.props.color}}></div>)
     }
 }
